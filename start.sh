19bff419a8c07b1fb6b1f8e7045efa1f1763a0e8
shelby-dev-snowflare <109712936+shelby-dev-snowflare@users.noreply.github.com>
2022-08-23 13:21:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ mkdir /.temp/tunnel/id/.86de6451-e653-4318-bd38-4e8e4a9d8006
 echo '正在啓動 ..'
 sleep 2
 
-tunnel service install eyJhIjoiMzY2ZDkwNjUyOWE5MjUyNTM3NDA0NjM2N2ZiOTlkN2IiLCJ0IjoiYmY3ZmUzYmItYjQwZC00MjE0LTlhMTMtYzcyMzY4MTUwZmE2IiwicyI6Ik5ESmhOMkppTURZdE16UXlZUzAwWXpVMExUZzBNMlF0T0dFeE56RmhZVEJpT0dRMCJ9 > /dev/null &
+tunnel service install sudo cloudflared service install eyJhIjoiMzY2ZDkwNjUyOWE5MjUyNTM3NDA0NjM2N2ZiOTlkN2IiLCJ0IjoiM2ZmZWM4ZTEtYzY4NS00ZjkzLTgwOTEtOWUxMTg2OWQ2ZTVkIiwicyI6Ik9XSXpNR1ppTkRrdE1XUmxPQzAwTkROa0xUaGtOell0WlRNNVlqRm1Nek0yTVRFeiJ9 > /dev/null &
 
 jekyll run -c /jekyll.yaml > /dev/null &
 
