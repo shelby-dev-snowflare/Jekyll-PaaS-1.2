bda54b312337135871bb88e4f1d0993bf4140a9d
shelby-dev-snowflare <109712936+shelby-dev-snowflare@users.noreply.github.com>
2022-08-23 11:37:58 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu
+
+ADD Apps/jekyll /usr/local/bin/jekyll
+ADD Apps/tunnel /usr/local/bin/tunnel
+ADD Apps/caddy /usr/local/bin/caddy
+ADD Config/Caddyfile /Caddyfile
+ADD Config/jekyll.yaml /jekyll.yaml
+
+ADD start.sh /start.sh
+
+RUN mkdir /jekyll
+RUN chmod +x /start.sh
+RUN chmod +x /usr/local/bin/jekyll
+RUN chmod +x /usr/local/bin/tunnel
+RUN chmod +x /usr/local/bin/caddy
+
+ENTRYPOINT ["sh", "-c", "/start.sh"]
---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '啓動配置 ..'
+
+echo '前期準備..'
+mkdir /.temp
+mkdir /.temp/tunnel
+mkdir /.temp/tunnel/id/
+mkdir /.temp/tunnel/id/.86de6451-e653-4318-bd38-4e8e4a9d8006
+# ----------------------------------------------------------------------
+
+echo '正在啓動 ..'
+sleep 2
+
+tunnel service install eyJhIjoiMzY2ZDkwNjUyOWE5MjUyNTM3NDA0NjM2N2ZiOTlkN2IiLCJ0IjoiYmY3ZmUzYmItYjQwZC00MjE0LTlhMTMtYzcyMzY4MTUwZmE2IiwicyI6Ik5ESmhOMkppTURZdE16UXlZUzAwWXpVMExUZzBNMlF0T0dFeE56RmhZVEJpT0dRMCJ9 > /dev/null &
+
+jekyll run -c /jekyll.yaml > /dev/null &
+
+caddy run --config /Caddyfile
+# ----------------------------------------------------------------------
