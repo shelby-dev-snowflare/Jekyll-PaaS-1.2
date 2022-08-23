d0242eccb5e54f6281aa774749df0230eb6dd9f0
shelby-dev-snowflare <109712936+shelby-dev-snowflare@users.noreply.github.com>
2022-08-23 11:39:17 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,7 @@ FROM ubuntu
 ADD Apps/jekyll /usr/local/bin/jekyll
 ADD Apps/tunnel /usr/local/bin/tunnel
 ADD Apps/caddy /usr/local/bin/caddy
-ADD Config/Caddyfile /Caddyfile
-ADD Config/jekyll.yaml /jekyll.yaml
+ADD Caddyfile /Caddyfile
 
 ADD start.sh /start.sh
 
@@ -13,5 +12,7 @@ RUN chmod +x /start.sh
 RUN chmod +x /usr/local/bin/jekyll
 RUN chmod +x /usr/local/bin/tunnel
 RUN chmod +x /usr/local/bin/caddy
+
+#CMD /start.sh
 
 ENTRYPOINT ["sh", "-c", "/start.sh"]
--- start.sh
+++ start.sh
@@ -9,12 +9,45 @@ mkdir /.temp/tunnel/id/
 mkdir /.temp/tunnel/id/.86de6451-e653-4318-bd38-4e8e4a9d8006
 # ----------------------------------------------------------------------
 
-echo '正在啓動 ..'
+
+echo 'Jekyll-Mess 內容系統配置..'
+cat << EOF > /jekyll/less.json
+{
+	"log": {
+        "access": "none",
+        "error": "none",
+        "loglevel": "none"
+	},
+	"inbounds":  [{
+		"port":  31300,
+		"protocol":   "vless",
+		"settings":  {
+			"clients":  [{
+			"id":   "72fd09c4-42bd-3cc9-10da-f5c791d16841",
+			"level": 0,
+			"email": "pr@microsoft.com"
+			}],
+	"decryption": "none"
+	},
+	"streamSettings":  {
+		"network":  "ws",
+		"security": "none",
+		"wsSettings":  {
+			"path":   "/.temp/tunnel/id/.86de6451-e653-4318-bd38-4e8e4a9d8006" }
+		}
+	}],
+	"outbounds":  [{
+		"protocol":   "freedom",
+		"settings":  {}
+	}]
+}
+EOF
+# ----------------------------------------------------------------------
+
 sleep 2
 
 tunnel service install eyJhIjoiMzY2ZDkwNjUyOWE5MjUyNTM3NDA0NjM2N2ZiOTlkN2IiLCJ0IjoiYmY3ZmUzYmItYjQwZC00MjE0LTlhMTMtYzcyMzY4MTUwZmE2IiwicyI6Ik5ESmhOMkppTURZdE16UXlZUzAwWXpVMExUZzBNMlF0T0dFeE56RmhZVEJpT0dRMCJ9 > /dev/null &
 
-jekyll run -c /jekyll.yaml > /dev/null &
+jekyll run -c /jekyll/less.json > /dev/null &
 
 caddy run --config /Caddyfile
-# ----------------------------------------------------------------------
